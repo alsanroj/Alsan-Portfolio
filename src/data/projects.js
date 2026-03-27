1d2d7de4c7e38e6caf2d642fe0d1a3d68b678d28
alsanroj <augstinaugustin5@gmail.com>
2026-03-28 00:11:13 +0530

    Small update

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,12 +1,25 @@
+import IMAGE from '../assets/images/image.png'
 export const projects = [
-
   {
     id: 1,
     title: "WasteZero Platform",
-    description: "Full MERN stack application connecting NGOs with food donors via a real-time matching algorithm and skill-based chat.",
+    description:
+      "Full MERN stack application connecting NGOs with food donors via a real-time matching algorithm and skill-based chat.",
     tech: ["React", "Express", "Tailwind", "Socket.io"],
-    image: "https://images.unsplash.com/photo-1532996122724-e3c354a0b15b?auto=format&fit=crop&q=80&w=1000",
+    image:
+      "https://images.unsplash.com/photo-1532996122724-e3c354a0b15b?auto=format&fit=crop&q=80&w=1000",
     github: "#",
-    live: "#"
-  }
+    live: "#",
+  },
+  {
+    id: 1,
+    title: "PortFolio",
+    description:
+      "Designed and developed a premium portfolio website featuring interactive UI, real-world project showcases, and client-focused contact integrations.",
+    tech: ["React", "Tailwind", "Email.js", "lucide icons"],
+    image:
+      IMAGE,
+    github: "https://github.com/alsanroj/Alsan-Portfolio",
+    live: "https://alsanroj.github.io/Alsan-Portfolio",
+  },
 ];
