4f34ca1b3989d6a6f7dc00c8301c1a2e48ed4455
Alsan Roj <augstinaugustin5@gmail.com>
2026-06-06 00:28:00 +0530

    projects update

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,4 +1,7 @@
-import IMAGE from '../assets/images/image.png'
+import IMAGE1 from "../assets/images/image1.png";
+import IMAGE2 from "../assets/images/image2.png";
+import IMAGE3 from "../assets/images/image3.png";
+import IMAGE4 from "../assets/images/image4.png";
 export const projects = [
   {
     id: 1,
@@ -6,20 +9,44 @@ export const projects = [
     description:
       "Full MERN stack application connecting NGOs with food donors via a real-time matching algorithm and skill-based chat.",
     tech: ["React", "Express", "Tailwind", "Socket.io"],
-    image:
-      "https://images.unsplash.com/photo-1532996122724-e3c354a0b15b?auto=format&fit=crop&q=80&w=1000",
-    github: "#",
-    live: "#",
+    image: IMAGE1,
+    github: "https://github.com/springboardmentor8486a/waste-zero-feb-team01",
   },
   {
-    id: 1,
+    id: 2,
     title: "PortFolio",
     description:
       "Designed and developed a premium portfolio website featuring interactive UI, real-world project showcases, and client-focused contact integrations.",
     tech: ["React", "Tailwind", "Email.js", "lucide icons"],
-    image:
-      IMAGE,
+    image: IMAGE2,
     github: "https://github.com/alsanroj/Alsan-Portfolio",
     live: "https://alsanroj.github.io/Alsan-Portfolio",
+  },
+  {
+    id: 3,
+    title: "Twin Axis Global Trade",
+    description:
+      "Built a premium export business website for Twin Axis Global Trade with luxury branding, responsive design, interactive animations, and client-focused lead generation.",
+    tech: [
+      "React",
+      "Vite",
+      "Tailwind CSS",
+      "Framer Motion",
+      "JavaScript",
+      "Lucide Icons",
+    ],
+    image: IMAGE3,
+    github: "https://github.com/twinaxis2316/TwinAxis",
+    live: "https://twin-axis.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Smart Xerox System",
+    description:
+      "Developed a smart college xerox management system with secure admin authentication, digital document handling, and streamlined print request workflows to reduce waiting time and improve operational efficiency.",
+    tech: ["React", "Node.js", "Express.js", "MongoDB", "JWT", "Tailwind CSS"],
+    image: IMAGE4,
+    github: "https://github.com/alsanroj/Smart-Xerox-System-for-College",
+    live: "",
   },
 ];
